427581ea691b68a5a805cf408454ac60cdde4fac
Zameer Hombali <zameerhombali01@gmail.com>
2025-09-21 12:02:58 +0530

    Update config.php to use env variables with SSL

--- connection/config.php
+++ connection/config.php
@@ -1,13 +1,16 @@
 <?php
-$host     = getenv('DB_HOST');
-$user     = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
-$database = getenv('DB_NAME');
-$port     = getenv('DB_PORT') ?: 3306;
+$host = getenv("DB_HOST");
+$username = getenv("DB_USER");
+$password = getenv("DB_PASS");
+$database = getenv("DB_NAME");
 
-$conn = @mysqli_connect($host, $user, $password, $database, (int)$port);
+$conn = mysqli_init();
 
-if (!$conn) {
-    die("DB connection failed: " . mysqli_connect_error());
+// Enable SSL (Azure requires it)
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+// Try connecting
+if (!mysqli_real_connect($conn, $host, $username, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
