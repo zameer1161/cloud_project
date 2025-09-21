ca69f1ab61e507a08c06d769bdf733527d9a81ff
Zameer Hombali <zameerhombali01@gmail.com>
2025-09-21 12:04:45 +0530

    Edit config.php to use Azure environment variables

--- connection/config.php
+++ connection/config.php
@@ -1,16 +1,21 @@
 <?php
 $host = getenv("DB_HOST");
-$username = getenv("DB_USER");
-$password = getenv("DB_PASS");
-$database = getenv("DB_NAME");
+$username = getenv("DB_USERNAME");
+$password = getenv("DB_PASSWORD");
+$dbname = getenv("DB_NAME");
+$port = getenv("DB_PORT");
 
+// Path to Azure CA certificate
+$cert = "/var/www/html/DigiCertGlobalRootCA.crt.pem";
+
+// Create connection
 $conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, $cert, NULL, NULL);
 
-// Enable SSL (Azure requires it)
-mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+// Turn off server cert validation if needed
+mysqli_real_connect($conn, $host, $username, $password, $dbname, $port, MYSQLI_CLIENT_SSL);
 
-// Try connecting
-if (!mysqli_real_connect($conn, $host, $username, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL)) {
-    die("Connection failed: " . mysqli_connect_error());
+if (mysqli_connect_errno()) {
+    die("Failed to connect to MySQL: " . mysqli_connect_error());
 }
 ?>
